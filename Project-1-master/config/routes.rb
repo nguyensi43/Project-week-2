cf2b835c6e420fba19e01efb66374d0501486b84
nguyensi <nguyensi43@gmail.com>
2016-12-16 07:26:42 +0700

    Use Ckeditor

---
+++ Project-1-master/config/routes.rb
@@ -0,0 +1,21 @@
+
+
+
+
+Rails.application.routes.draw do
+
+  mount Ckeditor::Engine => '/ckeditor'
+  get 'welcome/index'
+
+resources :articles do
+resources :comments
+
+
+end
+
+
+
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
