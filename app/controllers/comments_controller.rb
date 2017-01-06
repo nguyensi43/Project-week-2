40462aa039bbddb877c2aad15e7375503fa634f2
nguyensi <nguyensi43@gmail.com>
2017-01-06 17:05:57 +0700

    Comment with ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,18 @@ class CommentsController < ApplicationController
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
+      respond_to do |format|
+        if @article.comment.save
+                format.html { redirect_to('/', :notice => 'Line item was successfully created') }
+                format.js 
+                format.json { render :show, status: :created, location: @comment }
+            else
+                format.html { render :new }
+                format.json { render json: @line_item.errors, status: :unprocessable_entity }
+            end
+        format.js
+      end
   end
- 
   def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
